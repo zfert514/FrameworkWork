b69974a0f72f9819382ddf4cb15ceadf6e8cd67f
zfert514 <61804342+zfert514@users.noreply.github.com>
2021-07-05 11:02:21 +0300

    initial commit

---
+++ FrameworkWork/FrameworkWork.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkWork.h
+//  FrameworkWork
+//
+//  Created by Zachary Fertig on 6/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkWork.
+FOUNDATION_EXPORT double FrameworkWorkVersionNumber;
+
+//! Project version string for FrameworkWork.
+FOUNDATION_EXPORT const unsigned char FrameworkWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkWork/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '12.0'
+
+target 'FrameworkWork' do
+  use_frameworks!
+  #pod 'AWSPinpoint'
+  #pod 'AWSMobileClient'
+  pod 'Amplify'
+  #pod 'AmplifyPlugins/AWSDataStorePlugin'
+  #pod 'AmplifyPlugins/AWSAPIPlugin'
+	pod 'AWSPluginsCore'
+	pod 'AWSCognitoIdentityProvider'
+end
